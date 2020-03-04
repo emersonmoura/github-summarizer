c0aaae710eb68031edbdb8edee65e9cddf7eaf8d
emerson <emerson@unovation.com.br>
2020-03-04 19:52:39 -0300

    initial project structure

---
+++ src/main/scala/scalac/summarizer/Hello.scala
@@ -0,0 +1,9 @@
+package scalac.summarizer
+
+object Hello extends Greeting with App {
+  println(greeting)
+}
+
+trait Greeting {
+  lazy val greeting: String = "hello"
+}
---
+++ src/test/scala/scalac/summarizer/HelloSpec.scala
@@ -0,0 +1,9 @@
+package scalac.summarizer
+
+import org.scalatest._
+
+class HelloSpec extends FlatSpec with Matchers {
+  "The Hello object" should "say hello" in {
+    Hello.greeting shouldEqual "hello"
+  }
+}
