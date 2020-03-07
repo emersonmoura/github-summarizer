dca8cf96e8f464fc660f0493ad48f25c5700811f
emerson <emerson@unovation.com.br>
2020-03-07 17:04:47 -0300

    the etag cache was refactored

--- src/main/scala/scalac/summarizer/http/EtagCache.scala
+++ src/main/scala/scalac/summarizer/http/EtagCache.scala
@@ -1,15 +1,22 @@
 package scalac.summarizer.http
 
-import akka.http.scaladsl.model.HttpResponse
-import scalac.summarizer.integration.handler.PaginatedHandler.cache
+import java.util.concurrent.{ConcurrentHashMap, ConcurrentMap}
 
-object EtagExtractor {
+import akka.http.scaladsl.model.HttpResponse
+
+object EtagCache {
+
+  val cache: ConcurrentMap[String, String] = new ConcurrentHashMap
 
   val ETAG = "etag"
 
-  def processEtag[T](url: String, response: HttpResponse):Unit = {
+  def putIfAbsent[T](url: String, response: HttpResponse):Unit = {
     val etag = getEtag(response, url)
     etag.getOrElse(cache.put(url, _))
+  }
+
+  def get(url: String) = {
+    cache.get(url)
   }
 
   private def getEtag[T](response: HttpResponse, value: String) = {
--- src/main/scala/scalac/summarizer/integration/handler/PaginatedHandler.scala
+++ src/main/scala/scalac/summarizer/integration/handler/PaginatedHandler.scala
@@ -3,15 +3,13 @@ package scalac.summarizer.integration.handler
 import java.util.concurrent.{ConcurrentHashMap, ConcurrentMap}
 
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
-import scalac.summarizer.http.{EtagExtractor, HeaderFactory, HeaderLinkExtractor, HttpClient}
+import scalac.summarizer.http.{EtagCache, HeaderFactory, HeaderLinkExtractor, HttpClient}
 import scalac.summarizer.json.JsonSupport
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 object PaginatedHandler {
-
-  val cache: ConcurrentMap[String, String] = new ConcurrentHashMap
 
   implicit class Paginated(httpClient: HttpClient) extends JsonSupport {
 
@@ -22,14 +20,14 @@ object PaginatedHandler {
       def processPagination(response: HttpResponse): Future[Seq[T]] = {
         val link = HeaderLinkExtractor.extract(response.headers)
         val eventualValue = unmarshal(response)
-        EtagExtractor.processEtag(url, response)
+        EtagCache.putIfAbsent(url, response)
         link.map(value => processPaginatedRequest(value,unmarshal).zipWith(eventualValue)(_ ++ _)).getOrElse(eventualValue)
       }
       httpClient.sendRequest(HttpRequest(uri = url, headers = headers(url) )).flatMap(processPagination).fallbackTo(genericFallback)
     }
 
     private def headers[T](url: String) = {
-      HeaderFactory.gitHubHeaders(cache.get(url))
+      HeaderFactory.gitHubHeaders(EtagCache.get(url))
     }
   }
 
