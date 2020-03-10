282c13b16701dc5635031ea68403c9151c4d12f8
emerson <emerson@unovation.com.br>
2020-03-10 14:34:29 -0300

    the HeaderFactory was changed not to consider GH_TOKEN when it isn't present

--- src/main/scala/scalac/summarizer/http/HeaderFactory.scala
+++ src/main/scala/scalac/summarizer/http/HeaderFactory.scala
@@ -6,14 +6,20 @@ import scala.collection.immutable
 
 object HeaderFactory {
 
-  val token = scala.util.Properties.envOrElse("GH_TOKEN", "token" )
+  val token = scala.util.Properties.envOrNone("GH_TOKEN")
 
   def gitHubHeaders(etag: String) = {
-    immutable.Seq(
-      RawHeader("Accept", "application/vnd.github.v3+json"),
-      RawHeader("If-None-Match", if(etag == null) "" else etag ),
-      RawHeader("Authorization", s"token $token")
-    )
+    token.map(defaultHeaders(etag) ++ authHeader(_)).getOrElse(defaultHeaders(etag)).to[immutable.Seq]
   }
 
+  private def authHeader(token: String) = {
+    Seq(RawHeader("Authorization", s"token $token"))
+  }
+
+  private def defaultHeaders(etag: String) = {
+    Seq(
+      RawHeader("Accept", "application/vnd.github.v3+json"),
+      RawHeader("If-None-Match", if (etag == null) "" else etag)
+    )
+  }
 }
